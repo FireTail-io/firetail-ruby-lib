fa1adf237991517f0989430bacc6983c540a1eef
Muhammad Nuzaihan <muhammad@taqisystems.com>
2022-11-23 06:15:24 +0800

    code updates for the new API endpoints and schema

--- lib/firetail.rb
+++ lib/firetail.rb
@@ -38,12 +38,10 @@ module Firetail
       end
 
       raise Error.new "Please run 'rails generate firetail:install' first" if config.nil?
-      raise Error.new "Token is missing from firetail.yml configuration" if config['token'].nil?
       raise Error.new "API Key is missing from firetail.yml configuration"  if config['api_key'].nil?
 
-      @token              = config['token']
       @api_key            = config['api_key']
-      @url                = config['url'] ? config['url'] : "https://ingest.eu-west-1.dev.firetail.app/ingest/request" # default goes to dev
+      @url                = config['url'] ? config['url'] : "https://api.logging.eu-west-1.sandbox.firetail.app/logs/bulk" # default goes to dev
       @log_drains_timeout = config['log_drains_timeout'] ? config['log_drains_timeout'] : 5
       @network_timeout    = config['network_timeout'] ? config['network_timeout'] : 10
       @number_of_retries  = config['number_of_retries'] ? config['number_of_retries'] : 4
@@ -51,13 +49,13 @@ module Firetail
       # End of configuration initialization
 
       # Gets the rack middleware requests
-      request = Rack::Request.new(env)
+      @request = Rack::Request.new(env)
       started_on = Time.now
       begin
         status, client_response_headers, body = response = @app.call(env)
-        log(env, response, status, client_response_headers, body, started_on, Time.now)
+	log(env, response, status, client_response_headers, body, started_on, Time.now)
       rescue Exception => exception
-        log(env, response, status, client_response_headers, body, started_on, Time.now, exception)
+	      log(env, response, status, client_response_headers, body, started_on, Time.now, exception)
         raise exception
       end
 
@@ -82,51 +80,43 @@ module Firetail
       request_method            = env['REQUEST_METHOD']
       request_http_host         = env['HTTP_HOST']
       request_http_version      = env['HTTP_VERSION']
-      request_http_encoding     = env['HTTP_ACCEPT_ENCODING']
-      request_http_accept       = env['HTTP_ACCEPT']
       request_query_string      = env['QUERY_STRING']
       request_path              = env['REQUEST_PATH']
       request_uri               = env['REQUEST_URI']
-      request_server_software   = env['SERVER_SOFTWARE']
-      request_server_port       = env['SERVER_PORT']
-      request_gateway_interface = env['GATEWAY_INTERFACE']
-      request_http_connection   = env['HTTP_CONNECTION']
 
-      # get the request "HTTP_" headers
-      request_headers = env.select {|k,v| k.start_with? 'HTTP_'}
-       .collect {|key, val| [key.sub(/^HTTP_/, ''), val]}
-       .collect {|key, val| "#{key}: #{val}<br>"}
-       .sort
+      # select those with "HTTP_" prefix, these are request headers
+      request_headers = env.select {|key,val| key.start_with? 'HTTP_' } # find HTTP_ prefixes, these are requests only
+	                .collect {|key, val| { "#{key.sub(/^HTTP_/, '')}": [val] }} # remove HTTP_ prefix
+	                .reduce({}, :merge)  # flatten from [{key:val},{key2: val2}] to {key: val, key2: val2}
+
+      # do the inverse of the above and get rack specific keys
+      response_headers = env.select {|key,val| !key.start_with? 'HTTP_' } # only keys with no HTTP_ prefix
+	                 .select {|key, val| key =~ /^[A-Z._]*$/} # select keys with uppercase and underline
+                         .map {|key, val| { "#{key}": [val] }} # map to firetail api format
+                         .reduce({}, :merge) # flatten from [{key:val},{key2: val2}] to {key: val, key2: val2}
 
       # add the request and response data 
       # to array of data for batching up
       @reqres.push({
-	version: "1.1",
+	version: "1.0.0-alpha",
 	dateCreated: Time.now.utc.to_i,
-	execution_time: time_spent,
-	#dateCreated: 1663763942581,
-	#execution_time: 3.74,
-	req: {
+	executionTime: time_spent,
+        request: {
  	  httpProtocol: request_http_version,
-	  headers: request_headers.to_s,
-	  path: request_path,
+	  headers: request_headers, # headers must be in: headers: {"key": ["value"]}, array in object
 	  method: request_method,
-          oPath: request_path,
-	  fPath: request_path,
-	  args: request_query_string,
+	  body: @request.body.read,
 	  ip: request_ip,
-	  pathParams: request_path,
-	  user_agent: request_user_agent # maybe need this?
+	  resource: request_path,
+	  uri: "#{@request.url}"
 	},
-	resp: {
-          status_code: status,
-	  content_len: client_response_headers['Content-Length'],
-	  content_enc: client_response_headers['Content-Encoding'],
+	response: {
+          statusCode: status,
 	  body: body ? body.body : body[0],
-          headers: client_response_headers.to_s,
-	  content_type: client_response_headers['Content-Type'], 
-	  error_type: exception&.class&.name, # maybe need this? 
-          error_message: exception&.message # maybe need this? can be removed
+          headers: response_headers,
+	},
+	oauth: {
+          subject: "ThisIsAWorkInProgress",
 	}
       })
 
@@ -146,15 +136,11 @@ module Firetail
       # if there are more than 5 requests or is more than
       # 2 minutes, then send to backend - this is for testing
       if @reqres.length >= 5 || duration > 120
-        #Firetail.logger.debug "request data #{@request_data.length}"
-	payload = "" # begin of data will have a newline
-	@reqres.each do |data|
-          # append string in-place
- 	  json = data.to_json
-	  payload = json + "\n"
-        end
+        #Firetail.logger.debug "request data #{@reqres}"
+	# we parse the data hash into json-nl (json-newlines)
+	payload = @reqres.map { |data| JSON.generate(data) }.join("\n")
 
-	puts "Our data: #{payload}"
+	#puts "Our data: #{payload}"
 	# send the data to backend API
 	# This is an async task
 	Async do |task|
@@ -163,7 +149,7 @@ module Firetail
           end
 	end
 
-	# reset back tohe conditions
+	# reset back to the initial conditions
 	payload = nil
 	@reqres = []
 	@init_time = Time.now
@@ -179,16 +165,15 @@ module Firetail
 
       # Create a new request
       http = Net::HTTP.new(uri.hostname, uri.port)
-      http.set_debug_output($stdout)
+      #http.set_debug_output($stdout)
       http.use_ssl = true
       http.verify_mode = OpenSSL::SSL::VERIFY_PEER
 
       # Create a new request
       req = CustomPost.new(uri.path,
       {
-        'Content-Type': 'text/plain',
-        'x-api-key': @api_key,
-        'X-FT-API-KEY': @token
+        'content-type': 'application/nd-json',
+        'x-ft-api-key': @api_key
       })
 
       req.body = payload
