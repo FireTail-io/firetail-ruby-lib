b2deb3fa67cb9361ba8d161517be02f4abaa418b
Muhammad Nuzaihan Bin Kamal Luddin <muhammad@taqisystems.com>
2024-04-30 02:25:24 +0800

    fix deprecated methods, improve error handling, improve configuration file with defaults

--- spec/firetail_spec.rb
+++ spec/firetail_spec.rb
@@ -58,7 +58,7 @@ RSpec.describe Firetail do
 
   context "request to backend" do
     it "should have a correct request body" do
-      url = "https://api.logging.eu-west-1.sandbox.firetail.app/logs/bulk"
+      url = "https://api.logging.eu-west-1.prod.firetail.app/logs/bulk"
       api_key = "Abc123"
       payload = {"HTTP_ACCEPT"=>"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9", "HTTP_ACCEPT_ENCODING"=>"gzip, deflate, br", "HTTP_ACCEPT_LANGUAGE"=>"en-GB,en;q=0.9,ms-MY;q=0.8,ms;q=0.7,en-US;q=0.6,zh-CN;q=0.5,zh;q=0.4", "HTTP_CACHE_CONTROL"=>"no-cache", "HTTP_CONNECTION"=>"keep-alive", "HTTP_COOKIE"=>"_ga=GA1.1.1113729272.1635246362; intercom-id-ordh8mos=c5cc5a30-13b5-4869-a44f-ac645129e1c1; _ga_WT0V13SBWL=GS1.1.1662219522.5.1.1662220215.0.0.0; csrftoken=gk9Px9m28gi6XuIGBQaDaRgN9W19TadFhCvHPSwC3MYGH1QvnAK7EPpIidKVHLNa; _gcl_au=1.1.642239831.1666221572", "HTTP_DNT"=>"1", "HTTP_HOST"=>"localhost:3000", "HTTP_PRAGMA"=>"no-cache", "HTTP_SEC_CH_UA"=>"\"Google Chrome\";v=\"107\", \"Chromium\";v=\"107\", \"Not=A?Brand\";v=\"24\"", "HTTP_SEC_CH_UA_MOBILE"=>"?0", "HTTP_SEC_CH_UA_PLATFORM"=>"\"macOS\"", "HTTP_SEC_FETCH_DEST"=>"document", "HTTP_SEC_FETCH_MODE"=>"navigate", "HTTP_SEC_FETCH_SITE"=>"none", "HTTP_SEC_FETCH_USER"=>"?1", "HTTP_UPGRADE_INSECURE_REQUESTS"=>"1", "HTTP_USER_AGENT"=>"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36", "HTTP_VERSION"=>"HTTP/1.1"}.to_json
       headers = {
@@ -78,7 +78,7 @@ RSpec.describe Firetail do
     end
 
     it "should not be successful without the correct request body" do
-      url = "https://api.logging.eu-west-1.sandbox.firetail.app/logs/bulk"
+      url = "https://api.logging.eu-west-1.prod.firetail.app/logs/bulk"
       api_key = "Abc123"
       payload = {abc: 123}.to_json
       headers = {
@@ -107,7 +107,7 @@ RSpec.describe Firetail do
     end
 
     it "should be able to correctly run backoff strategy for failure from retries" do
-      url = "https://api.logging.eu-west-1.sandbox.firetail.app/logs/bulk"
+      url = "https://api.logging.eu-west-1.prod.firetail.app/logs/bulk"
       api_key = "Abc123"
       payload = {abc: 123}.to_json
       headers = {
@@ -132,7 +132,7 @@ RSpec.describe Firetail do
     end
 
     it "should be able to correctly run backoff strategy for failure from timeouts" do
-      url = "https://api.logging.eu-west-1.sandbox.firetail.app/logs/bulk"
+      url = "https://api.logging.eu-west-1.prod.firetail.app/logs/bulk"
       api_key = "Abc123"
       payload = {abc: 123}.to_json
       headers = {
